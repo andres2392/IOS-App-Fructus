a5211c6b4532fbd00d63f5f9957bbde03ae03a91
Andres <andres.gonzalez2392@gmail.com>
2022-08-23 12:10:07 -0400

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by PDWS on 8/23/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
